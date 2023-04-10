e52faaa1678c7e1e5066f52bccc6428256f7afdf
austin.walker <austin.walker.wgwx@statefarm.com>
2023-04-10 13:07:09 -0500

    GREEN: shouldBeAbleToRename

--- src/test/java/com/galvanize/collecties/collectie/species/StanTests.java
+++ src/test/java/com/galvanize/collecties/collectie/species/StanTests.java
@@ -6,21 +6,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Collectie: Stan")
 public class StanTests {
-    @DisplayName("Collectie: Stan")
-    public class StanTests {
 
-        Stan stanley;
+    Stan stanley;
 
-        @BeforeEach
-        public void testSetup() { stanley = new Stan(); }
+    @BeforeEach
+    public void testSetup() {
+        stanley = new Stan();
+    }
 
-        @Test
-        public void shouldBeAbleToRename() {
-            String newName = "Stanley";
-            stanley.setName(newName);
-            assertEquals(newName, stanley.getName());
+    @Test
+    public void shouldBeAbleToRename() {
+        String newName = "Stanley";
+        stanley.setName(newName);
+        assertEquals(newName, stanley.getName());
 
-        }
     }
 }
+
