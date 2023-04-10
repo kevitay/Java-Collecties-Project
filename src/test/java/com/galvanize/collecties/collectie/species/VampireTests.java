8a48decaf323384e74f846adc4ef1cb931ed629b
DevPhilC <philip.christman.jk3f@statefarm.com>
2023-04-10 14:27:03 -0500

    GREEN: added support to rename vampire collectie

--- src/test/java/com/galvanize/collecties/collectie/species/VampireTests.java
+++ src/test/java/com/galvanize/collecties/collectie/species/VampireTests.java
@@ -1,7 +1,9 @@
 package com.galvanize.collecties.collectie.species;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName("Collectie: Vampire")
 public class VampireTests {
@@ -12,5 +14,13 @@ public class VampireTests {
         testVampire = new Vampire();
     }
 
-
+    @Test
+    public void shouldBeAbleToRename(){
+        //setup
+        String newName = "Count Dracula";
+        //execute
+        testVampire.setName(newName);
+        //assessment
+        Assertions.assertEquals(newName, testVampire.getName());
+    }
 }
