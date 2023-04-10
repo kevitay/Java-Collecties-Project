198e08e75dc0299f4cc619df3ab0bbdafb4b13dd
austin.walker <austin.walker.wgwx@statefarm.com>
2023-04-10 15:29:41 -0500

    formatting cleanup

--- src/main/java/com/galvanize/collecties/collectie/species/CollectieSpecies.java
+++ src/main/java/com/galvanize/collecties/collectie/species/CollectieSpecies.java
@@ -1,4 +1,5 @@
 package com.galvanize.collecties.collectie.species;
+
 import com.galvanize.collecties.collectie.Collectie;
 
 /*
@@ -11,39 +12,38 @@ import com.galvanize.collecties.collectie.Collectie;
  */
 public enum CollectieSpecies {
 
-  // Each element in this enum requires
-  // a reference instance of the class
-  // it represents
-  // SPECIES(new Species()) / KEY(ReferenceValue)
-  REXTORE(new Rextore()),
-  DODUD(new Dodud()),
+    // Each element in this enum requires
+    // a reference instance of the class
+    // it represents
+    // SPECIES(new Species()) / KEY(ReferenceValue)
+    REXTORE(new Rextore()),
+    DODUD(new Dodud()),
+    STANLEY(new Stanley()),
+    INFERNIA(new Infernia()),
+    BOOGIEWOOGIE(new BoogieWoogie()),
+    GENGAR(new Gengar()),
+    FLUKOGUIN(new Flukoguin()),
+    TRICERATOPS(new Triceratops()),
+    GALVA(new Galva());
 
-  STANLEY(new Stanley()),
-  INFERNIA(new Infernia()),
-  BOOGIEWOOGIE(new BoogieWoogie()),
-  GENGAR(new Gengar()),
-  FLUKOGUIN(new Flukoguin()),
-  TRICERATOPS(new Triceratops()),
-  GALVA(new Galva());
+    /*
+     * Enums can actually have a constructor
+     * and instance data/methods. In this case
+     * we just need to keep a reference object
+     * for the consumable each enum represents
+     * This allows us to easy create clones of
+     * those reference objects attached to each enum
+     * Check out the code in the Consumables enum for
+     * usage examples
+     */
 
-  /*
-   * Enums can actually have a constructor
-   * and instance data/methods. In this case
-   * we just need to keep a reference object
-   * for the consumable each enum represents
-   * This allows us to easy create clones of
-   * those reference objects attached to each enum
-   * Check out the code in the Consumables enum for
-   * usage examples
-   */
+    private final Collectie reference;
 
-  private final Collectie reference;
+    CollectieSpecies(Collectie reference) {
+        this.reference = reference;
+    }
 
-  CollectieSpecies(Collectie reference) {
-    this.reference = reference;
-  }
-
-  public Collectie getReference() {
-    return reference;
-  }
+    public Collectie getReference() {
+        return reference;
+    }
 }
