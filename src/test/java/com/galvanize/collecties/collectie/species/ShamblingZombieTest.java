3466bd842d8b2d85570dc056284363247fd79d28
matheyc <chad.mathey.eg43@statefarm.com>
2023-04-10 15:21:01 -0500

    GREEN:  Passed final tests.

--- src/test/java/com/galvanize/collecties/collectie/species/ShamblingZombieTest.java
+++ src/test/java/com/galvanize/collecties/collectie/species/ShamblingZombieTest.java
@@ -1,5 +1,6 @@
 package com.galvanize.collecties.collectie.species;
 
+import com.galvanize.collecties.Biome;
 import com.galvanize.collecties.collectie.CollectieType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,4 +41,34 @@ public class ShamblingZombieTest {
         Assertions.assertNotEquals(0, graphic.length());
     }
 
+    @Test
+    public void shouldHaveTheCorrectSound() {
+        Assertions.assertEquals("A MIND IS A TERRIBLE THING TO WASTE", shamblingzombie.speak());
+    }
+
+    @Test
+    public void shouldPreferBogBiome() {
+        Assertions.assertEquals(Biome.BOG, shamblingzombie.getPreferredBiome());
+    }
+
+    @Test
+    public void shouldHave8AttackPower() {
+        Assertions.assertEquals(8, shamblingzombie.performAttack());
+    }
+
+    @Test
+    public void shouldSuccessfullyDefend50pOfTheTime() {
+        int wins = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (shamblingzombie.defend(0)) wins++;
+        }
+        System.out.println("Wins: " + wins++);
+        Assertions.assertTrue(wins > 450 && wins < 550);
+    }
+
+    @Test
+    public void shouldNewShamblingZombieOnClone() {
+        ShamblingZombie clone = shamblingzombie.clone();
+        Assertions.assertNotEquals(clone, shamblingzombie);
+    }
 }
