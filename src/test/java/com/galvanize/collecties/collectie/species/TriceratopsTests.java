3b8f00f44f3238f5cfa2ce76a588ac16f5565fd0
Michelle Showers <michelle.showers.ga72@statefarm.com>
2023-04-10 11:46:50 -0500

    Created Triceratops and TriceratopsTests and shouldBeAbleToRename test successful

---
+++ src/test/java/com/galvanize/collecties/collectie/species/TriceratopsTests.java
@@ -0,0 +1,26 @@
+package com.galvanize.collecties.collectie.species;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Collectie: Triceratops")
+public class TriceratopsTests {
+
+    Triceratops tops;
+
+    @BeforeEach
+    public void testSetup() {
+        tops = new Triceratops();
+    }
+
+@Test
+    public void shouldBeAbleToRename() {
+        String newName = "Tritops";
+        tops.setName(newName);
+        assertEquals(newName, tops.getName());
+}
+
+}
