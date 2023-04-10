2a09c3c129b065023a3b580455f06f7b6c3ff32c
Bob Nelson1971 <rln71@att.net>
2023-04-10 15:45:59 -0500

    wip

--- src/main/java/com/galvanize/collecties/consumable/Pizza.java
+++ src/main/java/com/galvanize/collecties/consumable/Pizza.java
@@ -2,7 +2,7 @@ package com.galvanize.collecties.consumable;
 
 public class Pizza implements Consumable {
 
-  private int slices = 4;
+  private int slices = 3;
 
   @Override
   public String getName() {
