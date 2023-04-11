f73cf67241761a7ee9560deef019de4587480117
austin.walker <austin.walker.wgwx@statefarm.com>
2023-04-11 10:13:44 -0500

    add CheesyPoofs consumable class

---
+++ src/main/java/com/galvanize/collecties/consumable/CheesyPoofs.java
@@ -0,0 +1,18 @@
+package com.galvanize.collecties.consumable;
+
+public class CheesyPoofs implements Consumable{
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean consume() {
+        return false;
+    }
+
+    @Override
+    public Consumable clone() {
+        return null;
+    }
+}
