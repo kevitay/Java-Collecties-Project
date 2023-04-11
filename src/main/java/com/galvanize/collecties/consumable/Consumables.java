0fd14e4a917b1dc7ad7de9be04f43c2a1dd139b3
austin.walker <austin.walker.wgwx@statefarm.com>
2023-04-11 10:41:31 -0500

    add CHEESYPOOFS to consumables.java

--- src/main/java/com/galvanize/collecties/consumable/Consumables.java
+++ src/main/java/com/galvanize/collecties/consumable/Consumables.java
@@ -29,7 +29,8 @@ public enum Consumables {
   MILK(new Milk()),
   FINGERFOOD(new FingerFood()),
   TRUBLOOD(new TruBlood()),
-  DINGHY (new Dinghy());
+  DINGHY (new Dinghy()),
+  CHEESYPOOFS (new CheesyPoofs());
 
 
 
